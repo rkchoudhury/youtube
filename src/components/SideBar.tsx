c622216d3d68e4508e7e603da0c62620662de683
rkchoudhury <rakeshchoudhury.rocky@gmail.com>
2024-03-12 23:21:33 +0530

    Added sidebar navigations

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,18 +1,39 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
-const menu_list = ["Home", "Shorts", "Subscriptions", "Explore"];
+import { ROUTE_NAMES } from "../navigation/Routes";
+
+const menu_list = [
+  {
+    label: "Home",
+    path: ROUTE_NAMES.HOME,
+  },
+  {
+    label: "Shorts",
+    path: ROUTE_NAMES.HOME,
+  },
+  {
+    label: "Subscriptions",
+    path: ROUTE_NAMES.HOME,
+  },
+  {
+    label: "Explore",
+    path: ROUTE_NAMES.HOME,
+  },
+];
 
 const SideBar = () => {
   return (
     <div className="w-40 px-6">
       <ul>
-        {menu_list.map((item) => (
-          <li
-            key={item}
-            className="p-2 border-0 rounded-lg hover:bg-gray-300 cursor-pointer"
-          >
-            {item}
-          </li>
+        {menu_list.map(({ label, path }) => (
+          <Link to={path}>
+            <li
+              key={label}
+              className="p-2 border-0 rounded-lg hover:bg-gray-300 cursor-pointer"
+            >
+              {label}
+            </li>
+          </Link>
         ))}
       </ul>
     </div>
